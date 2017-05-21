491056b9b3373842dbec588f6e07edbf5353b492
paulstey <paulstey@gmail.com>
2017-05-21 16:55:25 -0400

    first commit

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -1,6 +1,12 @@
 #!/anaconda/bin/python
 
+
+# NOTE: This code is still in progress. The swap_with_neighbor() method
+# does what is required, but now it needs to be applied to all elements
+
+
 from __future__ import print_function
+
 
 
 class Node:
@@ -13,37 +19,52 @@ class Node:
         self.neighbor = Node(neighbor_val)
 
     def has_neighbor(self):
-        res = True
-        
-        if self.neighbor == None:
-            res = False
-            #raise(BaseException, "Error: Node has no neighbor")
+        res = self.neighbor != None
         return res
 
+    def swap_with_neighbor(self):
+        third_neighbor = self.neighbor.neighbor
+        
+        self.neighbor.neighbor = self
+        print("Val: ", self.neighbor.neighbor.val)
+        self.neighbor = third_neighbor        
 
-def make_list(node, values, idx):
-    
+
+
+def growlist(node, values, idx):
+
     n_elems = len(values)
-    
+
     if idx < n_elems:
         node.add_neighbor(values[idx])
 
-        if idx + 1 < n_elems + 1:
-            make_list(node.neighbor, values, idx + 1)
+        if (idx + 1) < (n_elems + 1):
+            growlist(node.neighbor, values, idx + 1)
     else:
         return None
 
 
-def printlist(node, idx = 0):    
+def printlist(node, idx = 0):
     print("Node ", idx, ": ", node.val)
+
     if node.has_neighbor():
         printlist(node.neighbor, idx + 1)
 
 
 
+def reverselist(node, n_swaps = None):
+    if n_swaps == None:
+        node.swap_with_neighbor()
+    
+        
+
+        
+
 if __name__ == "__main__":
     xlist = Node(3.14)
-    make_list(xlist, [137, 2.71, 36, 41, 59], 0)
+    growlist(xlist, [137, 2.71, 36, 41, 59], 0)
     printlist(xlist)
-
+    new_list = xlist.neighbor
+    reverselist(xlist)
+    printlist(new_list)
     
