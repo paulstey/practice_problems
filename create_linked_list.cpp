ec6dd84761df8ce2e7fda0de930b85ebffc769db
paulstey <paulstey@gmail.com>
2017-06-14 12:16:26 -0400

    minor updates

--- create_linked_list.cpp
+++ create_linked_list.cpp
@@ -1,3 +1,19 @@
+#include <iostream>
+
+
+struct list {
+    int x;
+    int y;
+
+    list(int a, int b) : x(a), y(b) {}; 
+};
+
+int main(int argc, char *argv[]) {
+    list xlist = list(1, 2);
+
+    std::cout << xlist.x << " " << xlist.y << std::endl;
+}
+
 
 
 
