a62ac796fca814432e9a242da199ab335ce003b4
paulstey <paulstey@gmail.com>
2017-06-14 10:49:31 -0400

    complete re-write for ease of interpretation

--- square_root.cpp
+++ square_root.cpp
@@ -1,33 +1,24 @@
-// Square Root of Integer
-// Author: Paul Stey
-// Date: 2017-06-03
-// CXXFLAGS:
-// Description:
-//    Given an integer in the command line argument,
-//    this program returns the square root (or its floor).
-
 #include <iostream>
 
 int square_root(int n) {
-    if (n == 0 or n == 1) {
-	return n;
+    int soln = 1;
+    if (n == 0) {
+        return 0;
     }
-    int square = 1, i = 1, res = 1;
-    while (true) {
-	if (square == n) {
-	    res =  i;
-	    break;
+    else {
+	for (int i = 1; i < n; i++) {
+	    if (i*i == n) {
+		soln = i;
+		break;
+	    }
+	    else if (i*i > n) {
+		soln = i - 1;
+		break;
+	    }
 	}
-	else if (square > n) {
-	    res = i-1;
-	    break;
-	}
-	i++;
-	square = i*i;
     }
-    return res;
+    return soln;
 }
-
 
 int main(int argc, char *argv[]) {
     int n = atoi(argv[1]);
